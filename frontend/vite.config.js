662d1247576fc71a60d8f5c64d764a262c76c4f6
Bhargav Prasad <octagonemusic69@gmail.com>
2023-10-26 13:19:52 +0530

    Socket IO issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://flyrrchat.onrender.com/chats:5000",
         changeOrigin: true,
         secure: false,
       },
