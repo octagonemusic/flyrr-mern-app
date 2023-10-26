ddc678f123e9be94636610c61932e5619c18074a
Bhargav Prasad <octagonemusic69@gmail.com>
2023-10-26 13:02:20 +0530

    Fixed Socket IO issues

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://flyrrchat.onrender.com/",
+    origin: "*",
   },
 });
 
