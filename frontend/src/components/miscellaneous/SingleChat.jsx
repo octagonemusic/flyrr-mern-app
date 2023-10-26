ff6bf08f4be38cfd1d9662e97c4e673c0c4b96b9
Bhargav Prasad <octagonemusic69@gmail.com>
2023-10-26 13:15:23 +0530

    Fixed Socket IO issues

--- frontend/src/components/miscellaneous/SingleChat.jsx
+++ frontend/src/components/miscellaneous/SingleChat.jsx
@@ -17,7 +17,7 @@ import axios from "axios";
 import ScrollableChat from "./ScrollableChat";
 import io from "socket.io-client";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "http://https://flyrrchat.onrender.com:5000";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
