d266b3223ce63417875d2e2d57dfd2496fd44d13
pranav <venkatasaipranav2227@gmail.com>
2023-12-07 12:18:11 +0530

    Changes

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -25,34 +25,23 @@ const Homepage = () => {
 
   return (
     <Container maxW="xl" centerContent>
-      <Box
-        d="flex"
-        justifyContent="center"
-        p={3}
-        bg={"white"}
-        w="100%"
-        m="40px 0 15px 0"
-        borderRadius="1rem"
-        borderWidth="1px"
-      >
-        <Text textAlign={"center"} fontSize="4xl" fontFamily="Work sans">
-          Flyrr
+        <Text textAlign={"center"} fontWeight={"extrabold"} fontSize="50px" fontFamily="Montserrat" textColor={"black"} m="20px 0 0 0">
+          FLYRR
         </Text>
-      </Box>
 
       <Box
         d="flex"
         justifyContent="center"
         p={3}
-        bg={"white"}
+        bg={"#1E1E2E"}
         w="100%"
         m="40px 0 15px 0"
-        borderRadius="1rem"
+        borderRadius="2rem"
         borderWidth="1px"
       >
         <Tabs variant="soft-rounded" colorScheme="green">
           <TabList>
-            <Tab width="50%">Login</Tab>
+            <Tab width="50%" >Login</Tab>
             <Tab width="50%">Sign-Up</Tab>
           </TabList>
 
