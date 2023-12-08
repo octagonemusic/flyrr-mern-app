ed01deff21cbc7639b19c257e9a9f16eedef74b6
Bhargav Prasad <octagonemusic69@gmail.com>
2023-12-08 09:01:40 +0530

    Changed API Endpoints

--- frontend/src/components/miscellaneous/SingleChat.jsx
+++ frontend/src/components/miscellaneous/SingleChat.jsx
@@ -17,7 +17,7 @@ import axios from "axios";
 import ScrollableChat from "./ScrollableChat";
 import io from "socket.io-client";
 
-const ENDPOINT = "http://localhost:3000";
+const ENDPOINT = "//flyrrchat.onrender.com/";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
