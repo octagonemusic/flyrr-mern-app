56085c0e17f95dd132e853a3c08f13ed627a9bad
pranav <venkatasaipranav2227@gmail.com>
2023-12-11 11:53:05 +0530

    Login Page Commit

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -24,8 +24,8 @@ const Homepage = () => {
   }, [navigate]);
 
   return (
-    <Container maxW="xl" centerContent>
-        <Text textAlign={"center"} fontWeight={"extrabold"} fontSize="50px" fontFamily="Montserrat" textColor={"black"} m="20px 0 0 0">
+    <Container maxW="xl" centerContent backgroundColor={"#1E1E2E"}>
+        <Text textAlign={"center"} fontWeight={"extrabold"} fontSize="50px" fontFamily="Montserrat" textColor={"#E5E5E5"} m="20px 0 0 0">
           FLYRR
         </Text>
 
@@ -33,16 +33,16 @@ const Homepage = () => {
         d="flex"
         justifyContent="center"
         p={3}
-        bg={"#1E1E2E"}
+        bg={"#313244"}
         w="100%"
         m="40px 0 15px 0"
         borderRadius="2rem"
         borderWidth="1px"
       >
-        <Tabs variant="soft-rounded" colorScheme="green">
+        <Tabs variant="soft-rounded" colorScheme="purple">
           <TabList>
-            <Tab width="50%" >Login</Tab>
-            <Tab width="50%">Sign-Up</Tab>
+            <Tab width="50%" textAlign={"center"} fontWeight={"extrabold"} fontSize="15px" fontFamily="Montserrat" textColor={"#E5E5E5"} m="10px 0 0 0">Login</Tab>
+            <Tab width="50%" textAlign={"center"} fontWeight={"extrabold"} fontSize="15px" fontFamily="Montserrat" textColor={"#E5E5E5"} m="10px 0 0 0">Sign-Up</Tab>
           </TabList>
 
           <TabPanels>
