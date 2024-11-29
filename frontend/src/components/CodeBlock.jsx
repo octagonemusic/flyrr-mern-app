574780500da0c881fc087d51013faeb3ea984419
Bhargav Prasad <octagonemusic69@gmail.com>
2024-11-29 20:41:09 +0530

    Added additional language support to codeblocks

--- frontend/src/components/CodeBlock.jsx
+++ frontend/src/components/CodeBlock.jsx
@@ -3,27 +3,40 @@ import { Box, Text, useClipboard, IconButton } from "@chakra-ui/react";
 import { CopyIcon, CheckIcon } from "@chakra-ui/icons";
 import Prism from "prismjs";
 import "prismjs/themes/prism-tomorrow.css";
-import "prismjs/components/prism-javascript";
-import "prismjs/components/prism-python";
-import "prismjs/components/prism-java";
-import "prismjs/components/prism-jsx";
-import "prismjs/components/prism-typescript";
-import "prismjs/components/prism-css";
-import "prismjs/components/prism-json";
-import "prismjs/components/prism-bash";
-import "prismjs/components/prism-c";
-import "prismjs/components/prism-cpp";
-import "prismjs/components/prism-csharp";
-import "prismjs/components/prism-ruby";
-import "prismjs/components/prism-rust";
-import "prismjs/components/prism-sql";
-import "prismjs/components/prism-yaml";
-import "prismjs/components/prism-markdown";
-import "prismjs/components/prism-html";
-import "prismjs/components/prism-go";
-import "prismjs/components/prism-kotlin";
-import "prismjs/components/prism-swift";
-import "prismjs/components/prism-php";
+
+// Load core Prism components
+import "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+
+// Load specific languages
+const languages = [
+  "javascript",
+  "python",
+  "java",
+  "jsx",
+  "typescript",
+  "css",
+  "json",
+  "bash",
+  "c",
+  "cpp",
+  "csharp",
+  "ruby",
+  "rust",
+  "sql",
+  "yaml",
+  "markdown",
+  "html",
+  "go",
+  "kotlin",
+  "swift",
+  "php",
+];
+
+// Dynamically load language components
+languages.forEach((lang) => {
+  require(`prismjs/components/prism-${lang}`);
+});
 
 const CodeBlock = ({ code, language }) => {
   const { hasCopied, onCopy } = useClipboard(code);
