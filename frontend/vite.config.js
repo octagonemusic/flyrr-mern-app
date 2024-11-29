72be9587802f14268cc7570d52ca029e77da54fc
Bhargav Prasad <octagonemusic69@gmail.com>
2024-11-29 21:10:42 +0530

    Updated vite config to fix prismjs import

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["prismjs", "prismjs/themes/prism-tomorrow.css"],
-  },
-  build: {
-    rollupOptions: {
-      external: [
-        /^prismjs\/.*/, // This tells Vite to externalize all Prism.js imports
-      ],
-    },
-  },
   server: {
     host: true,
     port: 3000,
