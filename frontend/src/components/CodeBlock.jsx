b7e2c7ca257f6d5b9d7ba30467e6a5d0d321e2c5
Bhargav Prasad <octagonemusic69@gmail.com>
2024-11-29 21:14:11 +0530

    Updated codeblock styling

--- frontend/src/components/CodeBlock.jsx
+++ frontend/src/components/CodeBlock.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Box, Text, useClipboard, IconButton } from "@chakra-ui/react";
 import { CopyIcon, CheckIcon } from "@chakra-ui/icons";
 import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
 import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-python";
 import "prismjs/components/prism-java";
